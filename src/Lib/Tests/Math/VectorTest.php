e33aa2a6ffba6bda825f3e8548f22df026f6f828
Julien <julien@genudi.com>
2015-01-26 09:32:12 -0800

    File header format.

--- src/Lib/Tests/Math/VectorTest.php
+++ src/Lib/Tests/Math/VectorTest.php
@@ -1,9 +1,7 @@
 <?php
 
-/*
- * This file is part of the JulLib package.
- *
- * © 2014 Julien <youlweb@hotmail.com>
+/**
+ * JulLib ©2015 Julien <youlweb@hotmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
