86a011e3659e54e6a074603b7d393bbd030ca85c
youlweb <julientord@gmail.com>
2015-01-26 18:49:58 -0800

    Buffer class.

---
+++ src/Lib/Collection/Buffer/Buffer.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * JulLib ©2015 Julien <youlweb@hotmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Jul\Lib\Collection\Buffer;
+
+use Jul\Lib\Collection\Collection;
+
+/**
+ * A fixed size F.I.F.O object buffer.
+ *
+ * This implementation is based on the collection class.
+ * @author Julien <youlweb@hotmail.com>
+ */
+class Buffer extends Collection implements BufferInterface
+{
+    /** {@inheritDoc} */
+    public function add($object)
+    {
+        // TODO: Override collection method.
+    }
+
+    /** {@inheritDoc} */
+    public function first()
+    {
+        // TODO: Implement first() method.
+    }
+
+    /** {@inheritDoc} */
+    public function size()
+    {
+        // TODO: Implement size() method.
+    }
+}
