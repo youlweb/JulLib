bc199790902531268f7b64c630b81c8b4488f13b
youlweb <julientord@gmail.com>
2015-01-28 09:16:31 -0800

    String property interface.

---
+++ src/Lib/String/Property/PropertyInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * JulLib ©2015 Julien <youlweb@hotmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Jul\Lib\String\Property;
+
+/**
+ * Quantify a particular aspect of a string.
+ *
+ * @author Julien <youlweb@hotmail.com>
+ */
+interface PropertyInterface
+{
+    /**
+     * Return the property quantity for the input string.
+     *
+     * @param string $string
+     * @return float|int
+     */
+    public function value($string);
+}
