df7151796e3114fec7cb1b0cfcf5e1cbbd053e20
youlweb <julientord@gmail.com>
2015-01-30 09:24:58 -0800

    Whitespace  filter.

---
+++ src/Lib/String/Filter/Whitespace.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * JulLib ©2015 Julien <youlweb@hotmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Jul\Lib\String\Filter;
+
+/**
+ * Trim consecutive whitespaces down to a single whitespace.
+ *
+ * New lines and tabs are also converted to a single whitespace.
+ * Optionally, leading and trailing whitespaces may be stripped.
+ * @author Julien <youlweb@hotmail.com>
+ */
+class Whitespace implements FilterInterface
+{
+    /**
+     * @var bool
+     */
+    private $_trim;
+
+    /**
+     * @param bool $trim
+     */
+    public function __construct($trim = false)
+    {
+        $this->_trim = $trim;
+    }
+
+    /** {@inheritDoc} */
+    public function filter($string)
+    {
+        $filtered = preg_replace("/[\\x00-\\x20]+/", ' ', $string);
+        if ($this->_trim) {
+            return trim($filtered);
+        }
+        return $filtered;
+    }
+}
---
+++ src/Lib/Tests/String/Filter/WhitespaceTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * JulLib ©2015 Julien <youlweb@hotmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Jul\Lib\Tests\String\Filter;
+
+use Jul\Lib\String\Filter\Whitespace;
+
+/**
+ * @author Julien <youlweb@hotmail.com>
+ */
+class WhitespaceTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFilterWithoutTrim()
+    {
+        $whitespace = new Whitespace();
+        $this->assertEquals(' foo bar ', $whitespace->filter("\n\n\t   foo\t \n  bar  \n\n \t \r\r  "));
+    }
+
+    public function testFilterWithTrim()
+    {
+        $whitespace = new Whitespace(true);
+        $this->assertEquals('foo bar', $whitespace->filter("\n\n\t   foo\t \n  bar  \n\n \t \r\r  "));
+    }
+}
